06b1019e59c61267aae7fc737c7a4b185c534c2b
Mohana A <mohana.thangavelu@wavicledata.com>
2025-07-19 21:25:39 +0530

    Initial commit to vector-2 with GitHub Actions

--- vector_2/dev/mosot_dev/backend.tf
+++ vector_2/dev/mosot_dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  =  "mohana6-bucket"
-    key     =  "errorbudget-terraform-tfstate/vector-mosot-dev.tfstate"
+    key     =  "errorbudget-terraform-dev-tfstate/vector-mosot-dev.tfstate"
     region  =  "us-east-1"
     encrypt = true
     acl     = "private"
